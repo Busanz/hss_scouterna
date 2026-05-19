666025ad6219a66e662aa802e05b8f7f55967c52
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-05-19 22:20:14 +0200

    remove header

--- src/sections/Headersection.tsx
+++ src/sections/Headersection.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-//import { navLinks } from '../data/data';
-
 
 const Headersection = () => {
   return (
     <>
       <section>
-        <header className="w-full  h-[360px] mx-auto bg-[url('/images/header/header-bg.png')] bg-cover bg-center relative">
-        </header>
+        Header section
       </section>
     </>
   )
