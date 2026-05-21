bbd81eb57a1ba6e87edf9c131fbe51db54e1183e
Busanz <buddhisanz@gmail.com>
2026-05-22 01:08:51 +0200

    fix-merge-confit

--- src/data/data.ts
+++ src/data/data.ts
@@ -29,54 +29,6 @@ export const navLinks: NavLinksType = [
   },
 ];
 
-export const quickLinks: QuickLinkType[] = [
-  {
-    label: 'Bli scout',
-    image: '/images/footer/quicklinks/bliscout.svg',
-    href: '/bli-scout',
-  },
-
-  {
-    label: 'Avdelningar',
-    image: '/images/footer/quicklinks/avdelningar.svg',
-    href: '/avdelningar',
-  },
-  {
-    label: 'Sjöscout',
-    image: '/images/footer/quicklinks/sjöscout.svg',
-    href: '/sjoscout',
-  },
-  {
-    label: 'För föräldrar',
-    image: '/images/footer/quicklinks/förföräldrar.svg',
-    href: '/foraldrar',
-  },
-  { label: 'FAQ', image: '/images/footer/quicklinks/faq.svg', href: '/faq' },
-];
-
-export const socialMediaLinks: SocialLinkType[] = [
-  {
-    label: 'Facebook',
-    image: '/images/footer/socialmedia/facebook.svg',
-    href: 'https://www.facebook.com/scouterna',
-  },
-  {
-    label: 'Instagram',
-    image: '/images/footer/socialmedia/instagram.svg',
-    href: 'https://www.instagram.com/scouterna/',
-  },
-  {
-    label: 'LinkedIn',
-    image: '/images/footer/socialmedia/linkedin.svg',
-    href: 'https://se.linkedin.com/company/scouterna',
-  },
-  {
-    label: 'YouTube',
-    image: '/images/footer/socialmedia/youtube.svg',
-    href: 'https://www.youtube.com/channel/UCmFut5N4FcfLBJ70AhSJnmw',
-  },
-];
-
 export const introLinks: IntroLinksType = [
   {
     label: 'Om HSS',
