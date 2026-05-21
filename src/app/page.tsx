5518bad3a22841f967af032c6a4b7a6b942a2ba7
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-05-21 23:36:06 +0200

    set the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import Headersection from '@/sections/Headersection';
-import Footersection from '@/sections/Footersection';
+
 import History from '@/sections/History';
 import Introsection from '@/sections/Introsection';
 import OurCommitments from '@/sections/OurCommitments';
 export default function Home() {
   return (
     <div className="flex flex-col w-full h-full justify-center">
-      <Headersection />
       <Introsection />
       <History />
       <OurCommitments />
-      <Footersection />
     </div>
   );
 }
