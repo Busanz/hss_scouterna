cea243710707d9ce400ab719719243a90c306dcf
Aleksandra Ristovska <aleksandra@ristovska.com>
2026-05-21 12:38:11 +0200

    margin

--- src/sections/OurCommitments.tsx
+++ src/sections/OurCommitments.tsx
@@ -3,7 +3,7 @@ import Values from "@/components/Values"
 const OurCommitments = () => {
     return(
         <>
-            <section className="bg-[#003660] px-5 pb-15 md:px-30">
+            <section className="bg-[#003660] px-5 pb-15 mb-30 md:px-30">
                 <div className="my-16">
                     <h1 className="text-center text-3xl text-[#ffffff] mt-8 font-[Montserrat]">
                         Våra engagemang
