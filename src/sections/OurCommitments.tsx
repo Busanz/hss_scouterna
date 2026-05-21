388ed381a7e001aca2bf5b5edb254a8b52530b2f
Busanz <buddhisanz@gmail.com>
2026-05-22 01:33:23 +0200

    bugfix/add-margin

--- src/sections/OurCommitments.tsx
+++ src/sections/OurCommitments.tsx
@@ -2,8 +2,8 @@ import Values from '@/components/Values';
 
 const OurCommitments = () => {
   return (
-    <section className="flex flex-col w-full h-full items-center py-20">
-      <div className="flex flex-col w-full max-w-360 rounded-sm bg-primary">
+    <section className="flex flex-col w-full h-full items-center my-10 lg:my-20">
+      <div className="flex flex-col w-full max-w-360 rounded-sm pb-20 bg-primary">
         <div className="flex flex-col w-full my-16">
           <h1 className="text-center text-text-primary mt-8">
             Våra engagemang
