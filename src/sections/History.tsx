819d8d6d844d905736c574099d3e46837bf7c4d5
Busanz <buddhisanz@gmail.com>
2026-05-25 23:34:54 +0200

    Fix OS-HSS responsiveness and layout consitancy

--- src/sections/History.tsx
+++ src/sections/History.tsx
@@ -5,9 +5,9 @@ import OmHSSsection from './OmHSSsection';
 const History = () => {
   return (
     <section className="flex flex-col w-full h-full items-center">
-      <div className="flex flex-col w-full max-w-360 pt-20">
+      <div className="flex flex-col w-full max-w-360 pt-5 md:pt-10">
         <OmHSSsection />
-        <h1 className="text-center text-3xl mt-8 text-text-secondary">
+        <h1 className="text-center text-text-secondary text-2xl sm:text-3xl lg:text-4xl  pt-10 md:pt-20">
           Historia
         </h1>
         <hr className="my-8 border-primary" />
--- src/sections/OmHSSsection.tsx
+++ src/sections/OmHSSsection.tsx
@@ -3,10 +3,12 @@ import { omHSSLinks } from '@/data/data';
 
 const OmHSSsection = () => {
   return (
-    <section className="flex flex-col w-full h-fit items-center justify-center my-10">
-      <div className="flex flex-col items-center w-full h-full rounded-sm text-text-primary bg-primary">
-        <h1>Hässelby Strands Sjöscoutkår (HSS)</h1>
-        <div className="grid grid-cols-3 w-full max-w-360 text-text-primary px-4 md:px-6 lg:px-10 py-10 place-items-center">
+    <div className="flex flex-col w-full h-fit items-center justify-center">
+      <div className="flex flex-col items-center w-full h-full rounded-sm text-text-primary bg-bg-light">
+        <h1 className="text-center text-2xl sm:text-3xl lg:text-4xl mt-8 text-text-secondary py-5 md:py-10">
+          Hässelby Strands Sjöscoutkår (HSS)
+        </h1>
+        <div className="grid grid-cols-2 lg:grid-cols-3 w-full max-w-360 text-text-primary place-items-center pb-14 gap-2 md:gap-4 lg:gap-10 px-4">
           {omHSSLinks.map((item, index) => (
             <LinkedCard
               key={index}
@@ -17,7 +19,7 @@ const OmHSSsection = () => {
           ))}
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
