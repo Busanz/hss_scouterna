d97a187c5ad38bca326f18a51405a67c58353c24
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-05-26 22:01:23 +0200

    Feature/40-goodtohave

---
+++ src/app/(navlinks)/om-hss/klader-utrustning/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GoodToHave from "@/sections/GoodToHave";
+
+const KladerUtrustning = () => {
+    return(
+        <>  
+            <GoodToHave />
+        </>
+    )
+}
+export default KladerUtrustning;
+
+
---
+++ src/sections/GoodToHave.tsx
@@ -0,0 +1,28 @@
+const GoodToHave = () => {
+    return (
+        <>
+            <section className="m-4">
+                <div className="p-4 mb-5 sm:px-6 md:px-8">
+                    <h2 className="text-center text-2xl p-4 mb-4 sm:text-3xl md:text-4xl">Good To Have</h2>
+                    <h3 className="text-primary text-center sm:text-lg md:text-xl mt-2">Things that are good to have when you've been a member for a while.</h3>
+                </div>
+                <div className="mx-auto my-8 h-0.5 w-full  sm:my-12 bg-text-subtitle" />
+                <div className="color-background pt-2 pl-8" >
+                    <ul className="list-disc text-black text-sm sm:text-base">
+                        <li>Scout uniform (T-shirt, polo shirt, or scout shirt with scarf)</li>
+                        <li>Sleeping mat</li>
+                        <li>Sleeping bag</li>
+                        <li>A pair of hiking shoes, boots or ankle boots</li>
+                        <li>Cutlery</li>
+                        <li>A non-slip plate or bowl to eat from</li>
+                        <li>Water bottle</li>
+                        <li>Rainwear</li>
+                    </ul>
+                </div>
+            </section>
+        </>
+    )
+}
+export default GoodToHave;
+
+
