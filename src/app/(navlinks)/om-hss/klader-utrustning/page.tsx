782b07513603ca5ed1364b501a9c9830d1de03ee
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-05-26 22:18:19 +0200

    Feature/037-scout-uniform

---
+++ src/app/(navlinks)/om-hss/klader-utrustning/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ScoutUniform from "@/sections/ScoutUniform";
+
+const KladerUtrustning = () => {
+    return(
+        <>  
+            <ScoutUniform />
+        </>
+    )
+}
+export default KladerUtrustning
