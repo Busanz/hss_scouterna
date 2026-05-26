06864546ff69607569272ab829b6a4b201df5e53
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-05-26 23:14:24 +0200

    Feature/39-scout-badges

---
+++ src/app/(navlinks)/om-hss/klader-utrustning/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Scoutbages from "@/sections/ScoutBages";
+
+const KladerUtrustning = () => {
+    return(
+        <>  
+           <Scoutbages />
+        </>
+    )
+}
+export default KladerUtrustning
+
+
+
