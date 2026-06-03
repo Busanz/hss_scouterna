f6dc88544de12a6ccae2b588fd533584bce03cf0
Aleksandra Ristovska <aleksandra@ristovska.com>
2026-06-03 13:54:11 +0200

    Add images to our boats

--- src/components/OurBoats.tsx
+++ src/components/OurBoats.tsx
@@ -1,24 +1,25 @@
 import Image from 'next/image';
 
 const boats = [
-  '/img/ourboats-section/fiveBoats.jpg',
-  '/img/ourboats-section/vendela.jpg',
-  '/img/ourboats-section/cephalea.jpg'
+  '/img/ourboats-section/vendela.png',
+  '/img/ourboats-section/cephalea.png',
+  '/img/ourboats-section/threeBoats.png',
+  '/img/ourboats-section/fiveBoats.png'
 ];
 
 const OurBoats = () => {
   return (
-    <div className="w-full pt-5 md:pt-20">
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-        <div className="flex flex-col justify-start">
-          <h2 className="text-center text-xl sm:text-2xl my-5">Våra båtar</h2>
-          <p className="sm:text-lg my-10">
+    <div className="w-full pt-12 md:pt-20">
+      <div className="flex flex-col justify-start">
+        <h2 className="text-center text-xl sm:text-2xl my-5">Våra båtar</h2>
+        <p className="sm:text-lg my-10 px-4 lg:px-0">
             Kåren äger fem segelbåtar, tio optimistjollar, två 2-kronor, och en
             snabb följebåt. Många HSS:are har lärt sig att segla på någon av våra
             båtar. Allt för att kunna stegra utbildningen på ett utmanande och
             äventyrligt sätt.
-          </p>
-        </div>
+        </p>
+      </div>
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
         {boats.map((image, index) => (
           <Image
             key={index}
