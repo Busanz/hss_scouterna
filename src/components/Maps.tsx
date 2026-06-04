4e07820b4a3b4c5abc87f8612b8847b3703fb345
wineynia <gabiybiy@gmail.com>
2026-06-04 22:12:54 +0200

    Add Map Section

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,54 +1,20 @@
 'use client'
 
-import  { useEffect, useRef } from 'react'
-import * as maplibregl from 'maplibre-gl'
-import 'maplibre-gl/dist/maplibre-gl.css'
-interface MapProps {
-  center: [number, number]
-  zoom?: number
-  showMarker?: boolean
-  markerColor?: string
+interface StaticMapProps {
+  src: string,
+  alt?:string,
 }
-const Maps = ({ center, zoom = 16, showMarker = true, markerColor ='#2563EB'}: MapProps) => {
-  const mapContainerRef = useRef<HTMLDivElement>(null);
-  const mapRef = useRef<maplibregl.Map | null>(null);
 
-  useEffect(() => {
-    if (!mapContainerRef.current) return;
-    if (mapRef.current) return;
-
-    mapRef.current = new maplibregl.Map({
-      container: mapContainerRef.current,
-      style: 'https://tiles.openfreemap.org/styles/bright/style.json',
-      center,
-      zoom,
-      pitch: 0,
-    });
-
-    mapRef.current.addControl(new maplibregl.NavigationControl(), 'top-right');
-    
-    requestAnimationFrame(() => mapRef.current?.resize());
-    setTimeout(() => mapRef.current?.resize(), 200);
-
-    if (showMarker) {
-      new maplibregl.Marker({ color: markerColor })
-        .setLngLat(center)
-        .addTo(mapRef.current);
-    }
-
-    return () => {
-      if (mapRef.current) {
-        mapRef.current.remove();
-        mapRef.current = null;
-      }
-    };
-  }, [center, zoom, showMarker, markerColor]);
-
+const StaticMap = ({src, alt ='Map'}: StaticMapProps) => {
   return (
     <div className="relative w-full h-48 md:h-56 lg:h-64 rounded-md overflow-hidden bg-blue-100 border-2 border-blue-500">
-      <div ref={mapContainerRef} className="absolute inset-0" />
+    <img 
+      src={src}
+      alt={alt}
+      className="absolute inset-0 w-full h-full object-cover"
+    />
     </div>
-  );
-};
+  )
+}
 
-export default Maps;
+export default StaticMap
--- src/sections/ContactInfosection.tsx
+++ src/sections/ContactInfosection.tsx
@@ -4,28 +4,28 @@ import { motion } from 'motion/react';
 import { Mail, MapPin, Building, TreePine, Sailboat } from "lucide-react"
 import { fadeUpAnimation } from '@/utils/animation';
 import LocationCard from '@/components/LocationCard';
-import Maps from '@/components/Maps';
+import StaticMap from '@/components/Maps';
 
 const ContactInfosection = () => {
   return (
     <div className="flex flex-col w-full max-w-360 h-full bg-primary text-text-primary mt-10 md:mt-20 px-4 md:px-6 lg:px-10 py-5 md:py-10 mb-5 md:mb-10 rounded-sm">
 
-      <motion.h2
+      <motion.h1
         {...fadeUpAnimation}
         className="text-center text-2xl sm:text-3xl lg:text-4xl text-text-primary px-1 py-5"
       >
         Äventyr och kompisar!
-      </motion.h2>
+      </motion.h1>
 
       <h2 className="text-center md:text-xl font-semibold sm:text-lg pb-5">
         Har du några funderingar eller behöver du komma i kontakt med oss?
       </h2>
 
       <div className="flex flex-col lg:flex-row gap-6 lg:gap-10 xl:gap-14">
-        <div className="flex-1 space-y-6">
+        <div className="flex-1 min-h-0 space-y-6">
         
-          <div className="bg-background rounded-md shadow-md p-4 md:p-5 lg:p-6 flex flex-col gap-3">
-            <Maps center={[17.7585, 59.3515]} zoom={16} showMarker={true} />
+          <div className="bg-background rounded-md shadow-md p-4 md:p-5 lg:p-10 flex flex-col gap-5">
+            <StaticMap src="/img/maps/hss-map.jpg" alt="HSS map" />
             <h3 className="text-xl md:text-2xl font-semibold flex items-center gap-2 text-text-secondary">
               <span
                 className="
@@ -42,7 +42,7 @@ const ContactInfosection = () => {
               Våra lokaler ligger vid Hässelby Strandbad och i skogen mellan Lövsta och Gåseborg.
             </p>
             <a
-              href="https://www.google.com/maps/search/?api=1&query=Bruksvägen+27,+165+65+Hässelby"
+              href="https://maps.app.goo.gl/UnkzfdoqAYyeze3c8"
               target="_blank"
               rel="noopener noreferrer"
               className="btn bg-primary text-text-primary hover:opacity-90 inline-flex items-center gap-2"
@@ -55,12 +55,12 @@ const ContactInfosection = () => {
         <div className="flex-1 space-y-6">
 
           <div className="bg-background text-text-secondary rounded-md shadow-md p-4 md:p-5 lg:p-6">
-            <h3 className="text-2xl font-semibold mb-4 flex items-center gap-2">
+            <h3 className="text-2xl font-semibold pb-5 flex items-center gap-2">
               <span aria-hidden="true"><Mail className="w-6 h-6" /></span>E-post
             </h3>
 
             <p className="mb-2">
-              <span className="font-medium">Address: </span>
+              <span className="font-medium">Allmänt: </span>
               <a href="mailto:info@hss-scout.org" className="underline">
                 info@hss-scout.org
               </a>
@@ -92,7 +92,7 @@ const ContactInfosection = () => {
             icon={<TreePine className="w-6 h-6" />}
             description="I skogen mellan Lövsta och Gåseborg ligger Myset, vår stora och fina scoutstuga. Här har kåren en mängd aktiviteter såsom lägeråterträff, KårMästerskapen (KM), övernattningar och hajker."
             extra="Myset består av ett stort samlingsrum med brasa, ett väl tilltaget kök och två mindre ledarrum. Köket är utrustat med el- och vedspis, diskmaskin, mikrovågsugn och stort kylskåp — ett komplett kök som kan serva upp till 30 personer. Koordinater: N 59°23.816′ E 017°46.230′"
-            mapLink="https://www.google.com/maps/search/?api=1&query=59.396933,17.770500"
+            mapLink="https://maps.app.goo.gl/NrhJRgKg1e3bMs7m6"
           />
 
           <LocationCard
@@ -101,7 +101,7 @@ const ContactInfosection = () => {
             icon={<Sailboat className="w-6 h-6" />}
             description="Ruffen är vår lokal vid Hässelby Strandbad. Här har vi våra avdelningsmöten och den huserar även våra optimistjollar."
             extra="Vägbeskrivning: Från Sandviksvägen sväng ner på Hässelby Strandväg. Vid badet finns en parkering. Gå ner mot vattnet och vik av mot vänster (mot kraftvärmeverket). Den grågröna stugan (Ruffen) ligger snett mittemot bryggan."
-            mapLink="https://www.google.com/maps/place/H%C3%A4sselby+Strandbad"
+            mapLink="https://maps.app.goo.gl/UnkzfdoqAYyeze3c8"
           />
         </div>
       </div>
