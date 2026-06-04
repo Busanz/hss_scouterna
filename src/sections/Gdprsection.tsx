44ca0b54a97c5fae8d5879a2a2ff6b91298a6e02
wineynia <gabiybiy@gmail.com>
2026-06-04 12:59:41 +0200

    Formatting Policy page

--- src/sections/Gdprsection.tsx
+++ src/sections/Gdprsection.tsx
@@ -1,6 +1,6 @@
 const GdprSection = () => {
   return (
-    <section className="w-full max-w-360 py-12 px-6 md:px-12 bg-text-subtitle text-text-secondary">
+    <section className="w-full max-w-360 py-12 px-6 md:px-12 bg-primary text-text-primary">
       <div className="max-w-4xl mx-auto">
 
         <h2 className="text-2xl font-semibold mb-4">GDPR</h2>
