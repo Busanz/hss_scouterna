669c4b07f45291b267a7a44f1b5b611bacf1a5de
Busanz <buddhisanz@gmail.com>
2026-06-05 00:06:50 +0200

    contact us change border

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,20 +1,20 @@
-'use client'
+'use client';
 
 interface StaticMapProps {
-  src: string,
-  alt?:string,
+  src: string;
+  alt?: string;
 }
 
-const StaticMap = ({src, alt ='Map'}: StaticMapProps) => {
+const StaticMap = ({ src, alt = 'Map' }: StaticMapProps) => {
   return (
-    <div className="relative w-full h-48 md:h-56 lg:h-64 rounded-md overflow-hidden bg-blue-100 border-2 border-blue-500">
-    <img 
-      src={src}
-      alt={alt}
-      className="absolute inset-0 w-full h-full object-cover"
-    />
+    <div className="relative w-full h-48 md:h-56 lg:h-64 rounded-md overflow-hidden">
+      <img
+        src={src}
+        alt={alt}
+        className="absolute inset-0 w-full h-full object-cover"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default StaticMap
+export default StaticMap;
