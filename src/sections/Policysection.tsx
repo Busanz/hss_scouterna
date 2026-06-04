17d1a2760d2d1f964d6d80b3dc31d07995932c38
wineynia <gabiybiy@gmail.com>
2026-06-04 20:12:05 +0200

    Formatting padding

--- src/sections/Policysection.tsx
+++ src/sections/Policysection.tsx
@@ -1,11 +1,11 @@
 const PolicySection = () => {
   return (
-    <section className="w-full max-w-360 py-16 px-6 md:px-12 mt-10 md:mt-20 text-text-secondary bg-text-primary">
+    <section className="w-full max-w-360 py-16 px-6 md:px-12 mt-10 md:mt-20 text-text-secondary">
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-center text-4xl font-extrabold mb-4">Policys</h1>
+        <h1 className="text-center text-4xl font-extrabold pb-5">Policys</h1>
 
         <div className="space-y-4">
-          <h3 className="text-xl font-semibold mb-4">Fotopolicy</h3>
+          <h3 className="text-xl font-semibold pb-5">Fotopolicy</h3>
           <p>
             Vi använder bilder från verksamheten i syfte att marknadsföra scouting och vår kår. Om du eller ditt barn inte vill vara med på bild kan ni meddela oss så tar vi bort eller undviker publicering.
           </p>
--- src/sections/Protokollsection.tsx
+++ src/sections/Protokollsection.tsx
@@ -1,12 +1,12 @@
 const ProtokollSection = () => {
   return (
-    <section className="w-full max-w-360 py-16 px-6 md:px-12 mb-10 md:mb-20 text-text-secondary bg-text-primary">
+    <section className="w-full max-w-360 py-16 px-6 md:px-12 mb-10 md:mb-20 text-text-secondary">
       <div className="max-w-4xl mx-auto">
-        <h3 className="text-xl font-semibold mb-4">Protokoll and Stadgar</h3>
+        <h3 className="text-xl font-semibold pb-5">Protokoll and Stadgar</h3>
 
         <div className="space-y-6">
           <div>
-            <h3 className="text-xl font-semibold mb-4">Kårstämma</h3>
+            <h3 className="text-xl font-semibold pb-5">Kårstämma</h3>
             <ul className="list-disc ml-6 space-y-1">
               <li>2020-04-20</li>
               <li>2020-05-15</li>
@@ -15,7 +15,7 @@ const ProtokollSection = () => {
           </div>
 
           <div>
-            <h3 className="text-xl font-semibold mb-4">Styrelsemöten</h3>
+            <h3 className="text-xl font-semibold pb-5">Styrelsemöten</h3>
             <ul className="list-disc ml-6 space-y-1">
               <li>2020-04-20</li>
               <li>2020-05-15</li>
