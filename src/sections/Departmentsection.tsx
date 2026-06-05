8ccd5fa09b889393508c8f5228bd4d35f9e0a5ef
Busanz <buddhisanz@gmail.com>
2026-06-05 23:56:15 +0200

    Remove rover section

--- src/sections/Departmentsection.tsx
+++ src/sections/Departmentsection.tsx
@@ -5,6 +5,8 @@ import { departmentsIntro } from '@/data/department';
 import DepartmentIntro from '@/components/DepartmentIntro';
 import type { DepartmentIntroType } from '@/types/types';
 import { useState } from 'react';
+import { motion } from 'motion/react';
+import { fadeUpAnimation } from '@/utils/animation';
 
 const Departmentsection = () => {
   const [departmentIndex, setDepartmentIndex] = useState<number>(0);
@@ -15,9 +17,12 @@ const Departmentsection = () => {
     <div className="flex flex-col w-full h-full items-center mt-10 lg:mt-20">
       <div className="relative flex flex-col w-full h-full items-center">
         <div className="relative flex flex-col w-full max-w-360 py-5 md:pt-10 text-text-secondary rounded-sm bg-primary">
-          <h1 className="flex w-full h-full justify-center text-2xl sm:text-3xl lg:text-4xl text-text-primary">
+          <motion.h1
+            className="flex w-full h-full justify-center text-2xl sm:text-3xl lg:text-4xl text-text-primary"
+            {...fadeUpAnimation}
+          >
             Avdelningar
-          </h1>
+          </motion.h1>
           <div className="flex flex-wrap justify-evenly w-full max-w-360 px-4 md:px-6 lg:px-10 py-5 lg:py-10 gap-y-4 md:gap-y-8">
             {departmentLogos.map((department, index) => (
               <button
@@ -31,12 +36,12 @@ const Departmentsection = () => {
                     src={department.url}
                     alt={`Logo of ${department.des}`}
                     fill
-                    className="object-contain p-1"
+                    className="object-contain p-1 hover:scale-103"
                     loading="lazy"
                     sizes="(max-width: 640px) 60px,(max-width: 768px) 70px, (max-width: 1024px) 80px, 100px"
                   />
                 </div>
-                <h3 className="font-normal text-lg sm:text-xl pb-1 text-text-primary">
+                <h3 className="font-normal text-lg sm:text-xl pb-1 text-text-primary ">
                   {department.label}
                 </h3>
                 <p className="flex text-base sm:text-lg justify-center text-text-primary">
