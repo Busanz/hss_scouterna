1f1c8ffe8042e195d4e3b11a1f2d01f4af701f2f
Busanz <buddhisanz@gmail.com>
2026-06-08 02:52:28 +0200

    Add traslation to the landing page 02

--- src/i18n/locale.ts
+++ src/i18n/locale.ts
@@ -5,8 +5,5 @@ import { cookies } from 'next/headers';
 const LOCALE_COOKIE = 'NEXT_LOCALE';
 
 export async function setUserLocale(locale: 'sv' | 'en') {
-  (await cookies()).set(LOCALE_COOKIE, locale, {
-    maxAge: 60 * 60 * 24 * 365,
-    path: '/',
-  });
+  (await cookies()).set(LOCALE_COOKIE, locale);
 }
