c744f38fcf6f893613cb7b6cbf4311fe64114d0f
Aleksandra Ristovska <aleksandra@ristovska.com>
2026-06-08 02:31:47 +0200

    add CategoryType

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -42,6 +42,14 @@ export type DepartmentType = DepartmentLink[];
 export type OmHSSLinksType = OmHSSLinks[];
 export type ClubClothsType = ClubCloths[];
 
+export type CategoryType = 'Allt' | 'Policy' | 'Allmänt' | 'Övrigt';
+ 
+export type FaqItemType = {
+  question: string;
+  answer: string;
+  category: CategoryType;
+  showAnswer?: Boolean;
+};
 export type DepartmentIntroType = {
   id: number;
   image: string;
