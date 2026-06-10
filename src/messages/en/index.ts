864769e8f1b3a960aedbf472654f93c725f4d0a3
wineynia <gabiybiy@gmail.com>
2026-06-11 00:57:51 +0200

    Add useTranslations to this section

--- src/messages/en/index.ts
+++ src/messages/en/index.ts
@@ -3,7 +3,10 @@ import introSection from './introSection.json';
 import footer from './footer.json';
 import contact from './contact.json'
 import lager from './lager.json';
-import locationCardDetail from './locationCardDetail.json'
+import locationCardDetail from './locationCardDetail.json';
+import gdpr from './gdpr.json';
+import policy from './policy.json';
+import protocol from './protocol.json'
 
-const en = { nav, introSection, footer, contact, lager, locationCardDetail };
+const en = { nav, introSection, footer, contact, lager, locationCardDetail, gdpr, policy, protocol };
 export default en;
--- src/messages/sv/index.ts
+++ src/messages/sv/index.ts
@@ -4,6 +4,9 @@ import footer from './footer.json';
 import contact from './contact.json'
 import lager from './lager.json';
 import locationCardDetail from './locationCardDetail.json'
+import gdpr from './gdpr.json';
+import policy from './policy.json';
+import protocol from './protocol.json';
 
-const sv = { nav, introSection, footer, contact, lager, locationCardDetail };
+const sv = { nav, introSection, footer, contact, lager, locationCardDetail, gdpr, policy, protocol };
 export default sv;
