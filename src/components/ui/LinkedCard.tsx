df1e2a27ca15b2e2fb49a89481179558fcf62463
Busanz <buddhisanz@gmail.com>
2026-06-12 01:00:14 +0200

    fix/068-layout-issue-our-boats

--- src/components/ui/LinkedCard.tsx
+++ src/components/ui/LinkedCard.tsx
@@ -19,17 +19,17 @@ const LinkedCard = ({ href, label, url, description }: LinkedCardProps) => {
       className="flex flex-col w-full max-w-105 h-full px-5 py-3 md:py-5 z-10 cursor-pointer bg-background rounded-sm"
     >
       <Link href={href}>
-        <h3 className="font-light w-full pb-2 min-w-40 md:min-w-50 lg:min-w-56 text-text-secondary text-left text-xl lg:text-2xl whitespace-nowrap">
+        <h2 className="font-light w-full pb-2 min-w-40 md:min-w-50 lg:min-w-56 text-text-secondary text-left text-xl lg:text-2xl whitespace-nowrap">
           {label}
-        </h3>
+        </h2>
         <div className="relative w-full aspect-video overflow-hidden max-w-105 pb-3">
           <Image
             src={url}
             fill
-            loading="lazy"
             sizes="(max-w-768px) 100vw, (max-w-1024px) 33vw, 420px"
             className="w-full h-full object-cover center rounded-sm transition-all duration-300 hover:scale-102"
             alt={label}
+            loading="eager"
           />
         </div>
         {description && (
